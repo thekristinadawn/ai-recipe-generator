c708c5de9c962fd55cd8a4b4ec837d3a7edccc00
thekristinadawn <thekristinadawn@gmail.com>
2024-02-22 13:25:42 -0600

    added axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "test",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "bc96c7e4d1otea7a98c0307cfdf6305b";
+  let context =
+    "You are a recipe food expert. Your mission is to generate a 5 ingredients or less recipe. Make sure to follow the user instructions with the ingredient that they want to include. The recipe should be simple. ";
+  let prompt = `User instructions: Generate a recipe that includes ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
